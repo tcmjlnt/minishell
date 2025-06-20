3dea94f26d86a682a0cbae6d5399dc519832d3e9
Thomas Jacquelinet <tjacquel@f6r3s3.clusters.42paris.fr>
2025-06-20 11:59:55 +0200

    ajout valeur de retour bool check_arg_export + protection args[i][0] == \'=\'

--- src/builtins/builtin_export.c
+++ src/builtins/builtin_export.c
@@ -6,7 +6,7 @@
 /*   By: tjacquel <tjacquel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/19 15:15:31 by tjacquel          #+#    #+#             */
-/*   Updated: 2025/06/19 16:50:55 by tjacquel         ###   ########.fr       */
+/*   Updated: 2025/06/20 11:57:31 by tjacquel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,22 +30,27 @@ int check_arg_export(t_shell *shell, t_cmd *cmd)
 {
 	int	i;
 	int j;
+	t_bool	res;
+	(void) shell;
 
 	i = 1;
 	j = 0;
+	res = true;
 	while(cmd->args[i])
 	{
-		while(cmd->args[i][j] && cmd->args[i][j] != '=')
+		while(cmd->args[i][j] && cmd->args[i][j + 1] && cmd->args[i][j + 1] != '=')
 		{
-			if(!is_valid_keychar(cmd->args[i][j]) || ft_isdigit(cmd->args[i][0]))
+			if(!is_valid_keychar(cmd->args[i][j]) || ft_isdigit(cmd->args[i][0]) || cmd->args[i][0] == '=')
 			{
 				printf("export: `%s': not a valid identifier\n", cmd->args[i]);
+				res = false;
 				break ;
 			}
 			j++;
 		}
 		i++;
 	}
+	return (res);
 }
 
 int	ft_export(t_shell *shell, t_cmd *cmd, int fd)
@@ -59,8 +64,8 @@ int	ft_export(t_shell *shell, t_cmd *cmd, int fd)
 	// new_env = env_new(cmd->args[1])
 	if (!cmd->args[1])
 		print_env_export(shell->env);
-	if(check_arg_export(shell, cmd))
-			return (shell->exit_status =1, false);
+	if (!check_arg_export(shell, cmd))
+			return (shell->exit_status = 1, false);
 	// split jusqauu  '='
 	// ajouter le noeud
 	// noeud add back
