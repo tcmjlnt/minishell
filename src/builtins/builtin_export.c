abfaf8ba4bcd2b29d970f6d8158eebe6b6231321
Thomas Jacquelinet <tjacquel@f6r3s3.clusters.42paris.fr>
2025-06-20 16:18:36 +0200

    gestion de la cle deja existante --> update de la valeur associee en gardant le meme noeud

--- src/builtins/builtin_export.c
+++ src/builtins/builtin_export.c
@@ -6,7 +6,7 @@
 /*   By: tjacquel <tjacquel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/19 15:15:31 by tjacquel          #+#    #+#             */
-/*   Updated: 2025/06/20 14:29:25 by tjacquel         ###   ########.fr       */
+/*   Updated: 2025/06/20 16:17:19 by tjacquel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,11 @@ void	print_env_export(t_env *env)
 		printf("export %s=", env->key);
 		if (env->value)
 			printf("\"%s\"", env->value);
-		// else
-		// 	printf("\"\"");
+
 		printf("\n");
 		env = env->next;
 	}
 }
-
-
 
 size_t check_key_export(t_shell *shell, char *arg)
 {
@@ -48,6 +45,23 @@ size_t check_key_export(t_shell *shell, char *arg)
 	}
 	return (++j);
 }
+
+t_env	*check_existing_key(t_shell *shell, char *key)
+{
+	t_env	*env_curr;
+
+	if (!shell->env)
+		return (NULL);
+	env_curr = shell->env;
+	while(env_curr)
+	{
+		if (ft_strcmp(env_curr->key, key) == 0)
+			return (env_curr);
+		env_curr = env_curr->next;
+	}
+	return (NULL);
+}
+
 int	arg_to_env(t_shell *shell, char *arg, size_t n)
 {
 	(void) shell;
@@ -58,14 +72,22 @@ int	arg_to_env(t_shell *shell, char *arg, size_t n)
 	key = NULL;
 	value = NULL;
 	key = ft_strndup(arg, n);
+	if (!key)
+		return (false);
 	value = ft_strdup(arg + n + 1);
-
-	printf("key: `%s`, value: `%s`\n", key, value);
-	env_new_node = env_new(key, value, true);
-	env_add_back((t_env **)&shell->env, env_new_node);
-
-	return (1);
-
+	if (!value)
+		return (false);
+	env_new_node = check_existing_key(shell, key);
+	if (env_new_node)
+		update_env_value(shell->env, key, value);
+	else
+	{
+		env_new_node = env_new(key, value, true);
+		if (!env_new_node)
+			return (false);
+		env_add_back((t_env **)&shell->env, env_new_node);
+	}
+	return (true);
 }
 
 int	export_args(t_shell *shell, t_cmd *cmd)
@@ -87,7 +109,6 @@ int	export_args(t_shell *shell, t_cmd *cmd)
 		else
 		{
 			j = check_key_export(shell, cmd->args[i]);
-			printf ("arg[%td]: key_strlen=%td\n", i, j);
 			arg_to_env(shell, cmd->args[i], j);
 		}
 		i++;
@@ -98,22 +119,13 @@ int	export_args(t_shell *shell, t_cmd *cmd)
 
 int	ft_export(t_shell *shell, t_cmd *cmd, int fd)
 {
-	//t_env	*new_env;
-
-	(void) cmd;
 	(void) fd;
-	// printf("export ");
 
-	// new_env = env_new(cmd->args[1])
 	if (!cmd->args[1])
 		print_env_export(shell->env);
 	if (!export_args(shell, cmd))
 		return (false);
-	// split jusqauu  '='
-	// ajouter le noeud
-	// noeud add back
 	return (true);
 }
 
-// Implémenter la commande builtin `export`
-// Ajouter ou modifier une variable d’environnement
+// Attention a la valeur de retour ft_export, je ne sais pas a l'heure actuelle ce que je dois retourner pour gestion dune erreur en amont
