bf08f08a16da9adf8f4da9bdac8d6b47b4cc1465
Aurore Martinez <126690118+aurore-martinez@users.noreply.github.com>
2025-06-24 13:57:51 +0200

    Exit fix (#32)
    * rapide fixe ajout dans la condition pour print le exit uniquement quand il est seul en commande
    
    * fix exit OK
    
    ---------
    
    Co-authored-by: Aurore Martinez <aumartin@f6r3s4.clusters.42paris.fr>

--- src/builtins/builtin_exit.c
+++ src/builtins/builtin_exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_exit.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tjacquel <tjacquel@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aumartin <aumartin@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/13 11:41:41 by aumartin          #+#    #+#             */
-/*   Updated: 2025/06/21 20:41:43 by tjacquel         ###   ########.fr       */
+/*   Updated: 2025/06/24 13:53:28 by aumartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,8 @@ int	ft_exit(t_shell *shell, t_cmd *cmd, int fd)
 
 	(void)fd;
 	exit_code = shell->exit_status;
-	ft_putstr_fd("exit\n", STDERR_FILENO);
+	if (!cmd->prev && !cmd->next)
+		ft_putstr_fd("exit\n", STDERR_FILENO);
 	if (!cmd->args[1])
 	{
 		gc_mem(GC_FREE_ALL, 0, NULL, GC_NONE);
@@ -42,7 +43,7 @@ int	ft_exit(t_shell *shell, t_cmd *cmd, int fd)
 	}
 	if (!is_numeric(cmd->args[1]))
 	{
-		ft_putstr_fd("minishell: exit: numeric argument required\n", STDERR_FILENO);
+		ft_putstr_fd("minishell: exit: numeric argument required\n", 2);
 		gc_mem(GC_FREE_ALL, 0, NULL, GC_NONE);
 		exit(2);
 	}
@@ -51,7 +52,6 @@ int	ft_exit(t_shell *shell, t_cmd *cmd, int fd)
 		ft_putstr_fd("minishell: exit: too many arguments\n", STDERR_FILENO);
 		return (1);
 	}
-	// gc_free_all avec GC_NONE avant de quitter ?? -> YES
 	exit_code = ft_atoi(cmd->args[1]) % 256;
 	gc_mem(GC_FREE_ALL, 0, NULL, GC_NONE);
 	exit(exit_code);
