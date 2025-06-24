04da6c438d00e7dea337c29f43c596e067176c86
Aurore Martinez <aumartin@f6r3s4.clusters.42paris.fr>
2025-06-24 13:56:39 +0200

    fix exit OK

--- src/builtins/builtin_exit.c
+++ src/builtins/builtin_exit.c
@@ -6,7 +6,7 @@
 /*   By: aumartin <aumartin@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/13 11:41:41 by aumartin          #+#    #+#             */
-/*   Updated: 2025/06/24 13:34:20 by aumartin         ###   ########.fr       */
+/*   Updated: 2025/06/24 13:53:28 by aumartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,15 +34,16 @@ int	ft_exit(t_shell *shell, t_cmd *cmd, int fd)
 
 	(void)fd;
 	exit_code = shell->exit_status;
-	if (!cmd->args[1] && !cmd->prev && !cmd->next)
-	{
+	if (!cmd->prev && !cmd->next)
 		ft_putstr_fd("exit\n", STDERR_FILENO);
+	if (!cmd->args[1])
+	{
 		gc_mem(GC_FREE_ALL, 0, NULL, GC_NONE);
 		exit(exit_code);
 	}
 	if (!is_numeric(cmd->args[1]))
 	{
-		ft_putstr_fd("minishell: exit: numeric argument required\n", STDERR_FILENO);
+		ft_putstr_fd("minishell: exit: numeric argument required\n", 2);
 		gc_mem(GC_FREE_ALL, 0, NULL, GC_NONE);
 		exit(2);
 	}
