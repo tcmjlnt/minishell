600f4b284ecafc038f01945d572727934a652fb6
Aurore Martinez <aumartin@f6r3s4.clusters.42paris.fr>
2025-06-24 13:36:38 +0200

    rapide fixe ajout dans la condition pour print le exit uniquement quand il est seul en commande

--- src/builtins/builtin_exit.c
+++ src/builtins/builtin_exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_exit.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tjacquel <tjacquel@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aumartin <aumartin@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/13 11:41:41 by aumartin          #+#    #+#             */
-/*   Updated: 2025/06/21 20:41:43 by tjacquel         ###   ########.fr       */
+/*   Updated: 2025/06/24 13:34:20 by aumartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,9 +34,9 @@ int	ft_exit(t_shell *shell, t_cmd *cmd, int fd)
 
 	(void)fd;
 	exit_code = shell->exit_status;
-	ft_putstr_fd("exit\n", STDERR_FILENO);
-	if (!cmd->args[1])
+	if (!cmd->args[1] && !cmd->prev && !cmd->next)
 	{
+		ft_putstr_fd("exit\n", STDERR_FILENO);
 		gc_mem(GC_FREE_ALL, 0, NULL, GC_NONE);
 		exit(exit_code);
 	}
@@ -51,7 +51,6 @@ int	ft_exit(t_shell *shell, t_cmd *cmd, int fd)
 		ft_putstr_fd("minishell: exit: too many arguments\n", STDERR_FILENO);
 		return (1);
 	}
-	// gc_free_all avec GC_NONE avant de quitter ?? -> YES
 	exit_code = ft_atoi(cmd->args[1]) % 256;
 	gc_mem(GC_FREE_ALL, 0, NULL, GC_NONE);
 	exit(exit_code);
