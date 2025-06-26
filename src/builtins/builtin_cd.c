e28ff79b26d1fe0ec8698aec7606e245ca5ac9cc
Aurore Martinez <aumartin@f6r3s4.clusters.42paris.fr>
2025-06-26 16:29:17 +0200

    ajout restoration de OLDPWD ou par defaut /

--- src/builtins/builtin_cd.c
+++ src/builtins/builtin_cd.c
@@ -6,7 +6,7 @@
 /*   By: aumartin <aumartin@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/14 14:17:01 by aumartin          #+#    #+#             */
-/*   Updated: 2025/06/21 18:52:34 by aumartin         ###   ########.fr       */
+/*   Updated: 2025/06/26 16:28:18 by aumartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,34 @@ static int	handle_special_cases(t_cmd *cmd)
 	return (0);
 }
 
+/* si repertoire courrant a ette supprime
+on recup l'ancien ou par defaut aller à /   */
+
+static int	handle_deleted_cwd(t_shell *shell)
+{
+	char	*cwd;
+	char	*oldpwd;
+
+	cwd = getcwd(NULL, 0);
+	if (cwd)
+		return (free(cwd), 0);
+	ft_putstr_fd("minishell: cd: current directory deleted\n", STDERR_FILENO);
+	oldpwd = get_env_value(shell->env, "OLDPWD", GC_ENV);
+	if (oldpwd && chdir(oldpwd) == 0)
+	{
+		ft_putstr_fd("minishell: cd: restored to OLDPWD\n", STDERR_FILENO);
+		update_env_value(shell->env, "PWD", gc_strdup(oldpwd, GC_ENV));
+		return (0);
+	}
+	if (chdir("/") == 0)
+	{
+		ft_putstr_fd("minishell: cd: restored to /\n", STDERR_FILENO);
+		update_env_value(shell->env, "PWD", gc_strdup("/", GC_ENV));
+		return (0);
+	}
+	return (1);
+}
+
 /* handle_special_cases() traite cd seul, cd ~, cd -.
 ft_cd() applique getcwd
 chdir, met à jour OLDPWD et PWD en utilisant gc_strdup
@@ -51,6 +79,8 @@ int	ft_cd(t_shell *shell, t_cmd *cmd, int fd)
 	char	*new_pwd;
 
 	(void)fd;
+	if (handle_deleted_cwd(shell) != 0)
+		return (shell->exit_status = 1, 1);
 	if (handle_special_cases(cmd))
 		return (shell->exit_status = 1, 1);
 	old_pwd = getcwd(NULL, 0);
@@ -70,6 +100,5 @@ int	ft_cd(t_shell *shell, t_cmd *cmd, int fd)
 	update_env_value(shell->env, "PWD", gc_strdup(new_pwd, GC_ENV));
 	free(old_pwd);
 	free(new_pwd);
-	shell->exit_status = 0;
 	return (0);
 }
