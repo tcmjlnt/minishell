7a308b80253c2f8c396b424991a421d4e9319c7b
Thomas Jacquelinet <tjacquel@f6r3s6.clusters.42paris.fr>
2025-06-27 18:07:58 +0200

    norminette main.c

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aumartin <aumartin@42.fr>                  +#+  +:+       +#+        */
+/*   By: tjacquel <tjacquel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 11:50:16 by aumartin          #+#    #+#             */
-/*   Updated: 2025/06/27 16:45:49 by aumartin         ###   ########.fr       */
+/*   Updated: 2025/06/27 18:07:39 by tjacquel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,17 +25,13 @@ int	main(int ac, char **av, char **envp)
 	int		res;
 
 	(void)av;
-
 	if (ac != 1)
 		return (ft_putstr_fd("minsihell error, try ./minishell\n", 2), -1);
-
 	init_shell();
 	shell = get_shell();
-
 	env_from_envp(shell, envp);
 	ft_prompt(shell);
 	res = shell->exit_status;
 	gc_mem(GC_FREE_ALL, 0, NULL, GC_NONE);
-
 	return (res);
 }
